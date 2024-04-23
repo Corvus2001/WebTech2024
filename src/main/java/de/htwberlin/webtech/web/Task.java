3122f80cb6936e6324f410028ba239b79f922d27
Corvus <corvin.jaenike@student.htw-berlin.de>
2024-04-23 16:26:36 +0200

    Task Class added//entity hinzugefügt

---
+++ src/main/java/de/htwberlin/webtech/web/Task.java
@@ -0,0 +1,83 @@
+package de.htwberlin.webtech.web;
+
+import java.util.Date;
+
+public class Task{
+    private int id;
+    private String title;
+    private String description;
+    private String status;
+    private Date dueDate;
+
+    // Constructor
+    public Task(int id, String title, String description, String status, Date dueDate) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.status = status;
+        this.dueDate = dueDate;
+    }
+
+    // Getter and Setter for id
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    // Getter and Setter for title
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    // Getter and Setter for description
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    // Getter and Setter for status
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    // Getter and Setter for dueDate
+    public Date getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(Date dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    // Method to update the status of the task
+    public void updateStatus(String newStatus) {
+        setStatus(newStatus);
+    }
+
+    // Override toString method for easy printing of task details
+    @Override
+    public String toString() {
+        return "Task{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", status='" + status + '\'' +
+                ", dueDate=" + dueDate +
+                '}';
+    }
+}
+
